32b2f27065848eb8f730a792fe3d4c438e0332f4
Michael Shevelin <shevel55505@gmail.com>
2020-11-30 16:37:01 +0200

    Brackets check with stack

--- src/Stack/StackArray.java
+++ src/Stack/StackArray.java
@@ -1,17 +1,28 @@
 package Stack;
-
+/*Fields:
+*       size - size of the stack
+*       element - array of elements
+*       top - index of an element, entered a stack last*/
 public class StackArray<T> implements Stack<T>{
     private int size;
     private T[] element;
     private int top;
 
-
+/*Constructor:
+*           single parameter size - sets a capacity of inner array which is capacity of stack itself
+*           initializes and inner array with fixed size, according to a parameter
+*           sets top to be -1, represents an empty array, since negative index in array is illegal*/
     StackArray(int size){
         this.size = size;
         element =(T[]) new Object[size];
         top = -1;
     }
-
+/*push method appends an element to the end of array
+since top is increments with each push, last element
+always will be indexed by top. It fits fine a first push, sine top
+was initialized as -1
+Complexity - O(1)
+*/
     @Override
     public void push(Object newElement) {
         if(isFull())
@@ -21,9 +32,11 @@ public class StackArray<T> implements Stack<T>{
                 e.printStackTrace();
             }
         element[++top] = (T) newElement;
-
     }
-
+/*Decreasing the top is enough to remove an elemnt from stack
+* since top always points at last element
+* also returns a pop'd element as required
+* complexity - O(1)*/
     @Override
     public T pop() {
         if(isEmpty())
@@ -40,11 +53,15 @@ public class StackArray<T> implements Stack<T>{
     }
 
     @Override
+    /*enough that isEmpty method would return true,
+    * to claim that Stack is empty
+    * Setting top to be its initial value -1
+    * defines a return value of isEmpty
+    * complexity - O(1)*/
     public void clear() {
-        while (!isEmpty()){
-            pop();
-        }
+        top = -1;
     }
+
 
     @Override
     public boolean isEmpty() { return top < 0;}
@@ -54,7 +71,8 @@ public class StackArray<T> implements Stack<T>{
     public boolean isFull() {
         return top + 1 == size;
     }
-    int getSize(){return size;}
+
+    int getSize(){ return size; }
 
     public String toString(){
         StringBuilder s;
--- src/Stack/checkBalancedBrackets.java
+++ src/Stack/checkBalancedBrackets.java
@@ -1,23 +1,35 @@
 package Stack;
 
 public class checkBalancedBrackets {
-    private final String left_brackets;
-    private final String right_brackets;
-    private StackList<Character> stack;
+    private final Character[] open = {'{','[','(','<'};
+    private final Character[] close = { '}', ']', ')', '>' };
+    private StackList<Integer> stack;
 
     checkBalancedBrackets(int testedStringLenght) {
-        left_brackets = "{[(<";
-        right_brackets = "}])>";
-        stack = new StackList<Character>(testedStringLenght);
+        stack = new StackList<Integer>(testedStringLenght);
     }
-
+/*Iterating over a given string and checking:
+*   if it is opening bracket, push its corresponding index in array of chars - defined as a field
+*   if it is a closing bracket, pop the stack and compare the indexes of current closing bracket with poped. It should fin*/
     public boolean check(String str) {
         for (int i = 0; i < str.length(); i++) {
-            if (in(str.charAt(i), left_brackets))
-                stack.push(str.charAt(i));
-            if (in(str.charAt(i), right_brackets)) {
-                if (stack.isEmpty() || str.charAt(i) != stack.pop())
-                    return false;
+            for (int j = 0; j < open.length; j++)
+            {
+                if(str.charAt(i) == open[j])
+                {
+                    stack.push(j);
+                    break;
+                }
+                if (str.charAt(i) == close[j])
+                {
+                    if(stack.isEmpty()) return false;
+                    if (stack.pop() == j)
+                    {
+                        break;
+                    }
+                    else
+                        return false;
+                }
             }
         }
 
@@ -33,10 +45,10 @@ public class checkBalancedBrackets {
     }
 
     public static void main(String[] args) {
-        String tested = "< f(x), g(x) > = int_{-/pi)^{/pi}f(x)g(x)dx";
-        String tested2 = "{]";
+        String tested = "< f(x), g(x) > = int_{-/pi}^{/pi}f(x)g(x)dx";
+        String tested2 = "{d}";
         checkBalancedBrackets brChecker = new checkBalancedBrackets(tested.length());
-        System.out.println(brChecker.check(tested2));
+        System.out.println(brChecker.check(tested));
 
     }
 
